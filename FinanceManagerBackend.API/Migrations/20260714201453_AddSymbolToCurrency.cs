84b2548d050f93cb2dad08f9721a0455a8bdbef8
Chernykh Artyom <chernykhav@bcs.ru>
2026-07-14 23:15:11 +0300

    add currency symbol migration

---
+++ FinanceManagerBackend.API/Migrations/20260714201453_AddSymbolToCurrency.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinanceManagerBackend.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSymbolToCurrency : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "symbol",
+                table: "currencies",
+                type: "character varying(3)",
+                maxLength: 3,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "symbol",
+                table: "currencies");
+        }
+    }
+}
